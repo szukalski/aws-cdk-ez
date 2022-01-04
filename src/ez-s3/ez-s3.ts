75a15694257f3fdad8003dca1b8add040e4503bc
David Roberts <szukalski@gmail.com>
2022-01-04 13:29:24 +0100

    Added EZ S3 Deployment

--- src/ez-s3/ez-s3.ts
+++ src/ez-s3/ez-s3.ts
@@ -1,11 +1,30 @@
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { BlockPublicAccess, Bucket, BucketEncryption, BucketProps } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 /**
  * Definition of S3 Bucket
  */
 export interface IEzS3BucketProps {
+  /**
+   * Source content to deploy to the S3 Bucket.
+   * 
+   * This can be either a local .zip file, or a local directory. Ie. './src/file.zip'. Contents of a .zip will be extracted.
+   * 
+   * @default - undefined
+   */
+  deploySource?: string;
+
+  /**
+   * Deployment prefix.
+   * 
+   * Source content will be deployed with this key prefix, ie. 'web/static'
+   * 
+   * @default - undefined
+   */
+  deployKeyPrefix?: string;
+
   /**
    * S3 Bucket props for the native Bucket object, these will take precedence.
    */
@@ -29,6 +48,15 @@ export class EzS3Bucket extends Construct {
       autoDeleteObjects: true,
       ...props?.bucketProps,
     });
+
+    if (props?.deploySource) {
+      new BucketDeployment(this, 'BucketDeploy', {
+        sources: [Source.asset(props.deploySource)],
+        destinationBucket: this.bucket,
+        destinationKeyPrefix: props?.deployKeyPrefix ?? '/',
+      })
+    };
+
     new CfnOutput(
       this,
       'BucketName', {
