92852be00c0de108b2dd625ca799cb60f8ec3174
Dave Roberts <szukalski@gmail.com>
2023-06-15 10:07:46 +0200

    update

--- .projenrc.js
+++ .projenrc.js
@@ -9,7 +9,7 @@ const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'David Roberts',
   authorAddress: 'szukalski@gmail.com',
-  cdkVersion: '2.83.1',
+  cdkVersion: '2.84.0',
   defaultReleaseBranch: 'main',
   name: 'aws-cdk-ez',
   license: 'MIT',
